ff0a9d5cf398e967507c53b7643a413924442d43
Júlio <drawzal@yahoo.com>
2016-07-13 02:52:28 +0100

    adjust to the develop branch

--- viewer/js/gis/dijit/StreetView/nls/pt-pt/resource.js
+++ viewer/js/gis/dijit/StreetView/nls/pt-pt/resource.js
@@ -1,10 +1,8 @@
 // http://dojotoolkit.org/reference-guide/1.10/dojo/i18n.html
 define({
-	activateButton: {
-        label: 'Clique no mapa para activar'
-    },
     messages: {
-        notAvailable: 'O Street View não esta disponível nesta Localização.'
+        instructions: 'Clique no botão do StreetView e depois clique na localização desejada no mapa.',
+        notAvailable: 'Infelizmente, o Google Street View não está disponível nesta localização.'
     },
     rightClickMenuItem: {
         label: 'Street View aqui'
